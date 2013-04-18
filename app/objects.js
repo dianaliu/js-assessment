677e644312fdb64358e637298d34191e1f8a357c
Diana Liu <dianaliu@nyu.edu>
2013-04-18 12:23:30 -0400

    Mark up objects.js per answers

--- app/objects.js
+++ app/objects.js
@@ -3,6 +3,7 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
+    	// fn.call(obj) works too. Difference is in how they pass arguments.
     	return fn.apply(obj);
     },
 
@@ -13,10 +14,17 @@ define(function() {
     iterate : function(obj) {
     	var ret = [];
 
-    	var props = Object.getOwnPropertyNames(obj);
-    	for(var i = 0; i < props.length; i++) {
-    		ret.push(props[i] + ": " + obj[props[i]]);
+    	// obj is a hash, could've just iterated over it.
+    	for(var p in obj) {
+    		if(obj.hasOwnProperty(p)) {
+    			ret.push(p + ": " + obj[p]);
+    		}
     	}
+
+    	// var props = Object.getOwnPropertyNames(obj);
+    	// for(var i = 0; i < props.length; i++) {
+    	// 	ret.push(props[i] + ": " + obj[props[i]]);
+    	// }
 
     	return ret;
     }
