6c21ceaf3161c5f97c1eeb2cf2c72572a8398f9b
Diana Liu <dianaliu@nyu.edu>
2013-04-18 12:19:16 -0400

    Fix objects.js

--- app/objects.js
+++ app/objects.js
@@ -3,15 +3,22 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+    	return fn.apply(obj);
     },
 
     alterObjects : function(constructor, greeting) {
-
+    	constructor.prototype.greeting = greeting;
     },
 
     iterate : function(obj) {
+    	var ret = [];
 
+    	var props = Object.getOwnPropertyNames(obj);
+    	for(var i = 0; i < props.length; i++) {
+    		ret.push(props[i] + ": " + obj[props[i]]);
+    	}
+
+    	return ret;
     }
   };
 });
