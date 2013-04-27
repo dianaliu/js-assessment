f19f83e44cf5bac88af676e2d164c5008a285a3c
Diana Liu <dianaliu@nyu.edu>
2013-04-26 22:42:41 -0400

    Update numbers per answer key

--- app/numbers.js
+++ app/numbers.js
@@ -3,9 +3,10 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     valueAtBit: function(num, bit) {
-        var binary = num.toString(2);
-        var value = binary.charAt(binary.length - bit);
-        return  parseInt(value, 10);
+        // Clever!
+        // Shift num so the bit you're interested in is in the right most place.
+        // 1 is 00..001 so &ing will return the value of the bit we want!
+        return 1 & (num >> (bit - 1));
     },
 
     base10: function(str) {
@@ -21,10 +22,20 @@ define(function() {
     },
 
     multiply: function(a, b) {
-        // turn decimals into whole numbers, divide by 10... at the end.
-        var bPlaces = b.toString().length - b.toString().indexOf('.') - 1;
-        var m = Math.pow(10, bPlaces);
-        var d = Math.pow(10, bPlaces * 2);
+        var getPlaces = function(num) {
+            if(num < 1) {
+                return num.toString().length - num.toString().indexOf('.') - 1;
+            } else {
+                return 0;
+            }
+        };
+
+        var aPlaces = getPlaces(a);
+        var bPlaces = getPlaces(b);
+        var places = Math.max(aPlaces, bPlaces);
+
+        var m = Math.pow(10, places);
+        var d = Math.pow(10, places * 2);
 
         var ret = ((a * m) * (b * m)) / d;
         return ret;
